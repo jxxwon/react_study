0e3dea679a4170146bba52209deaf1fa57599979
jxxone <wao_14@naver.com>
2024-07-26 17:20:31 +0900

    #01 공지사항 검색 컴포넌트 등록

---
+++ src/component/page/Notice/NoticeSearch/NoticeSearch.tsx
@@ -0,0 +1,7 @@
+export const NoticeSearch = () => {
+    return (
+        <>
+            NoticeSearch
+        </>
+    );
+}
--- src/routers/Routers.tsx
+++ src/routers/Routers.tsx
@@ -2,6 +2,8 @@ import { RouteObject, createBrowserRouter } from 'react-router-dom';
 import { Login } from '../pages/Login';
 import { DashBoard } from '../component/layout/DashBoard/DashBoard';
 import { NotFound } from '../component/common/NotFound/NotFound';
+import { Children } from 'react';
+import { Notice } from '../pages/Notice';
 
 const routers: RouteObject[] = [
     { path: '*', element: <NotFound /> },
@@ -9,6 +11,7 @@ const routers: RouteObject[] = [
     {
         path: '/react',
         element: <DashBoard />,
+        children: [{path: 'system', children: [{path: 'notice.do', element: <Notice/>}]}],
     },
 ];
 
